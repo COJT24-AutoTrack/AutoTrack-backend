47e2a367dff831649c3f627acc4b7c109b56477d
iorin-io <iodere77@gmail.com>
2024-06-08 16:06:19 +0900

    NOT NULLを追加

--- db/init.sql
+++ db/init.sql
@@ -19,7 +19,7 @@ CREATE TABLE Cars (
     car_name VARCHAR(255) NOT NULL,
     carmodelnum VARCHAR(255) NOT NULL,
     car_color VARCHAR(255) NOT NULL,
-    car_milage INT NOT NULL,
+    car_mileage INT NOT NULL,
     car_isflooding BOOLEAN NOT NULL,
     car_issmoked BOOLEAN NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -27,8 +27,8 @@ CREATE TABLE Cars (
 );
 
 CREATE TABLE user_car (
-    user_id INT,
-    car_id INT,
+    user_id INT NOT NULL,
+    car_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (car_id) REFERENCES Cars(car_id),
     PRIMARY KEY (user_id, car_id),
@@ -38,7 +38,7 @@ CREATE TABLE user_car (
 
 CREATE TABLE Tunings (
     tuning_id INT AUTO_INCREMENT PRIMARY KEY,
-    car_id INT,
+    car_id INT NOT NULL,
     tuning_name VARCHAR(255) NOT NULL,
     tuning_date DATE NOT NULL,
     tuning_description TEXT NOT NULL,
@@ -50,7 +50,7 @@ CREATE TABLE Tunings (
 
 CREATE TABLE Maintenances (
     maint_id INT AUTO_INCREMENT PRIMARY KEY,
-    car_id INT,
+    car_id INT NOT NULL,
     maint_type VARCHAR(255) NOT NULL,
     maint_date DATE NOT NULL,
     maint_description TEXT NOT NULL,
@@ -62,11 +62,11 @@ CREATE TABLE Maintenances (
 
 CREATE TABLE FuelEfficiencies (
     fe_id INT AUTO_INCREMENT PRIMARY KEY,
-    car_id INT,
+    car_id INT NOT NULL,
     fe_date DATE NOT NULL,
     fe_amount FLOAT NOT NULL,
     fe_unitprice FLOAT NOT NULL,
-    fe_milage INT NOT NULL,
+    fe_mileage INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (car_id) REFERENCES Cars(car_id),
@@ -75,7 +75,7 @@ CREATE TABLE FuelEfficiencies (
 
 CREATE TABLE Accidents (
     accident_id INT AUTO_INCREMENT PRIMARY KEY,
-    car_id INT,
+    car_id INT NOT NULL,
     accident_date DATE NOT NULL,
     accident_description TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -86,7 +86,7 @@ CREATE TABLE Accidents (
 
 CREATE TABLE PeriodicInspection (
     pi_id INT AUTO_INCREMENT PRIMARY KEY,
-    car_id INT,
+    car_id INT NOT NULL,
     pi_name VARCHAR(255) NOT NULL,
     pi_date DATE NOT NULL,
     pi_nextdate DATE NOT NULL,
